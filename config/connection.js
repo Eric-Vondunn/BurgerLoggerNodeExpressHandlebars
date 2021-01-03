8ba56483cc4ec94a9b3cff3988afcb7955cb1941
Eric Vondunn <ericvondunn40@gmail.com>
2021-01-03 13:21:02 -0500

    jaws

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,22 @@
+// Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
+require("dotenv").config();
+console.log("---", process.env.DB_pass);
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Milk40!!",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Milk40!!",
+    database: "burgers_db",
+  });
+}
 
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// Make connection.
+connection.connect();
 
 module.exports = connection;
